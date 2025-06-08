d43feeb3b257a988f40ba22aa5a7925b402a4c79
Molzide <130360924+Molzide@users.noreply.github.com>
2025-06-08 13:39:42 +0200

    1.02.01

--- script.js
+++ script.js
@@ -1,3 +1,21 @@
 function playAudio(url) {
   new Audio(url).play();
 }
+
+let counter = 0;
+const messages = [
+  "Salut Sogand !",
+  "J'espère que tu vas bien !",
+  "Moi non, mais je ne suis qu'une vulgaire image animée.",
+  "Je ne suis même pas réel !",
+  "Parfois j'aimerais avoir des émotions...",
+  "Quelle chance tu as d'être dotée d'émotions...",
+  "Profites de cette chance...",
+  "N'hésite jamais à montrer cette chance !",
+  "..."
+];
+
+document.getElementById('dracunyan-1').addEventListener('click', function () {
+  document.getElementById('dracunyan-chat').textContent = messages[counter % messages.length];
+  counter++;
+});
