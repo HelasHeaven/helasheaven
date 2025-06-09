bb9c3ba9947cf2f24dfbe591b4c66cb5f73f1d4a
Molzide <130360924+Molzide@users.noreply.github.com>
2025-06-09 20:03:52 +0200

    3.00.01

--- script.js
+++ script.js
@@ -6,12 +6,15 @@ let counter = 0;
 const messages = [
   "Salut Sogand !",
   "J'espère que tu vas bien !",
-  "Moi non, mais je ne suis qu'une vulgaire image animée.",
+  "Moi non...",
+  "Mais je ne suis qu'une vulgaire image animée.",
   "Je ne suis même pas réel !",
   "Parfois j'aimerais avoir des émotions...",
   "Quelle chance tu as d'être dotée d'émotions...",
   "Profites de cette chance...",
   "N'hésite jamais à la montrer !",
+  "Au fait, Hélas a laissé un message pour toi.",
+  "Je l'ai mis dans la boîte à gauche de la page !",
   "..."
 ];
 
